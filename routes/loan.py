75ac7d7f88f5b1c9152064f2885a733e4b890b41
Raj-Keshri398 <keshriraj093@gmail.com>
2026-05-21 22:07:15 +0530

    Enhance auth flow and redesign login/register pages

--- routes/loan.py
+++ routes/loan.py
@@ -1,14 +1,10 @@
 from flask import Blueprint
 
-loan_bp = Blueprint(
-    "loan",
-    __name__
-)
 
-@loan_bp.route(
-    "/loan"
-)
+loan_bp = Blueprint("loan", __name__)
 
+
+@loan_bp.route("/loan")
+@loan_bp.route("/loans")
 def loan():
-
-    return "Loan Page"
+    return "Loan page is under development."
